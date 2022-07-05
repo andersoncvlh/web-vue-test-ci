a99363730b523241cbce97f428cd5b9633ef3f5f
Anderson Carvalho Martins <CIANDT\andersoncm@lnb034996bhz.ciandt.global>
2022-07-05 15:23:42 -0300

    feat: testando mudança

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        Anderson
       </nav>
     </div>
   </header>
